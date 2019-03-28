70295723cd5f807ac6d7ceec757178021d0ad238
jesse <jesse.collier97@gmail.com>
2019-03-28 21:02:47 +1300

    just testing package.json

--- routes.js
+++ routes.js
@@ -7,10 +7,10 @@ const results = require('./results')//.music
 
 router.get('/', (req, res) => {
   //load main search page
+  res.render('partials/form');
 })
 
 router.post('/', (req, res) => {
-  //takes the 
 
   let formInfo = req.body;// will include genre, mood, decade
   //formInfo.genre, formInfo.mood, formInfo.year
@@ -21,18 +21,23 @@ router.post('/', (req, res) => {
   });
   //overwrite to results.json
   //this is where the matching algorithm goes
-  fs.writeFile()
-  //reroutes to results page
+  fs.writeFile('results.json', result, (err) => {
+    if (err) throw err;
+    //redirect to the page
+    res.redirect('/results')
+  })
 
 
 })
 
 router.get('/about', (req, res) => {
   //static about page
+  res.render('partials/about')
 })
 
 router.get('/results', (req, res) => {
   //gets the results.json data and then forms the page off that
+  res.render('partials/results', results)
 })
 
 module.exports = router
