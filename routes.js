7d552305a314031a6c1ea53207dae5f3a9e2bfb6
runminty <zengzhenhuang@gmail.com>
2019-03-29 15:16:45 +1300

    css changes

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
 const data = require('./data')//.music
 const results = require('./results')//.music
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
+
 server.use('/', routes)
 
 module.exports = server
